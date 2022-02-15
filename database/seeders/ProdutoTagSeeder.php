d19a93705e50eee83f6928f95703b31c149bbcb5
dcastanho44 <dcastanho44@gmail.com>
2022-02-15 17:40:43 -0300

    more seeders

---
+++ database/seeders/ProdutoTagSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\ProdutoTag;
+
+class ProdutoTagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      /*  $produtotag = new ProdutoTag();
+        $produtotag->produto_id = 1;
+        $produtotag->tags_id = 1;
+        $produtotag->produto_id = 1;
+        $produtotag->tags_id = 2;
+        $produtotag->save();
+
+        ProdutoTag::create([
+            'produto_id' => '2', 
+            'produto_id' => '2', 
+            'tags_id' => '2',
+            'tags_id' => '3',
+        ]); */
+
+    }
+}
