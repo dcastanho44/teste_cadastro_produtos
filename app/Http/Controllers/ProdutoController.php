6de8db0a02c22de83151d8639c4904d0eb3e1296
dcastanho44 <dcastanho44@gmail.com>
2022-02-17 16:16:43 -0300

    avançando no projeto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produto;
+use App\Models\ProdutoTag;
+use App\Models\User;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
@@ -14,7 +17,7 @@ class ProdutoController extends Controller
      */
     public function index(Request $request)
     {
-        $produtos = Produto::paginate(10); 
+        $produtos = Produto::paginate(15); 
         
         return view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all() ]);
     }
@@ -26,7 +29,8 @@ class ProdutoController extends Controller
      */
     public function create()
     {
-        //
+        $tags = Tag::all();
+        return view ('app.produto.create', ['tags' => $tags]);
     }
 
     /**
@@ -37,7 +41,34 @@ class ProdutoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
+        /*$regras = [
+            'name' => 'required|min:3|max:40',
+            'tag_id' => 'exists:tags,id'
+        ];
+
+        $feedback = [
+            'required' => 'O campo:attribute deve ser preenchido',
+            'name.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+            'tag_id.exists' => 'A tag precisa ser informada'
+        ];
+
+        $request->validate($regras, $feedback); */
+        $nome = $request->input("name");
+        $produto = Produto::create(["name" => $nome]);
+
+        $tags = $request->input("tags");
+            if(isset($tags)){
+                foreach($tags as $tag){
+                    ProdutoTag::create([
+                        "produto_id" => $produto->id,
+                        "tags_id" => $tag
+                    ]);
+                }
+            }
+
+        return redirect()->route('produto.index');
+        
     }
 
     /**
@@ -59,7 +90,8 @@ class ProdutoController extends Controller
      */
     public function edit(Produto $produto)
     {
-        //
+        $tags = Tag::all();
+        return view('app.produto.edit', ['produto' => $produto, 'tags' => $tags]);
     }
 
     /**
@@ -71,7 +103,20 @@ class ProdutoController extends Controller
      */
     public function update(Request $request, Produto $produto)
     {
-        //
+        $regras = [
+            'name' => 'required|min:3|max:40',
+        ];
+
+        $feedback = [
+            'name.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+            'name.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'name.required' => 'O campo precisa ser preenchido'
+        ];
+
+        $request->validate($regras, $feedback);
+        
+        $produto->update($request->all());
+        return redirect()->route('produto.index', ['produto' => $produto->id]);
     }
 
     /**
